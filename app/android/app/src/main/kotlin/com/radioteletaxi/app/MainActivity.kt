652cc1078637094bbe2f6c35c2bd95052246a262
Owen Perez <owenps123@gmail.com>
2026-02-16 17:17:03 -0500

    chore: init Flutter project

---
+++ app/android/app/src/main/kotlin/com/radioteletaxi/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.radioteletaxi.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- rtt_app/rtt_app/android/app/src/main/kotlin/com/radioteletaxi/rtt_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.radioteletaxi.rtt_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
